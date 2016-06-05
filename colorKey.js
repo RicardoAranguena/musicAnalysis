04d35f961c058e304f6c968613eabf14af5f2118
Malika Mataeva <a1063171@unet.univie.ac.at>
2016-06-05 17:54:27 +0200

    habe funktion colorKey extra in eine js gespeichert fuer ordnung

---
+++ colorKey.js
@@ -0,0 +1,15 @@
+function colorKey(key) {
+            if 		(key ===0) {return "rgb(31,120,180)"	}
+            else if (key ===1) {return "rgb(166,206,227)"	}
+            else if (key ===2) {return "rgb(106,61,154)"	}
+            else if (key ===3) {return "rgb(202,178,214)"	}
+            else if (key ===4) {return "rgb(177,89,40)"		}
+            else if (key ===5) {return "rgb(227,26,28)"		}
+            else if (key ===6) {return "rgb(251,154,153)"	}
+            else if (key ===7) {return "rgb(255,127,0)"		}
+            else if (key ===8) {return "rgb(253,191,111)"	}
+            else if (key ===9) {return "rgb(51,160,44)"		}
+            else if (key ===10) {return "rgb(178,223,138)"	}
+            else if (key ===11) {return "rgb(255,255,153)"	}
+            else{ return "black" };
+		};
